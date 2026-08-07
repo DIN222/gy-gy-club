6c95ca833647e7780a60dd15394348cb2ed09719
DIN222 <46632078+DIN222@users.noreply.github.com>
2026-08-07 21:02:23 +0300

    Add EventBus for module communication
    Implement EventBus for module communication with emit, on, and off methods.

--- core/eventBus.js
+++ core/eventBus.js
@@ -1,1 +1,19 @@
+// core/eventBus.js
+// «Материнская плата» проекта для связи модулей между собой
 
+export const EventBus = {
+    // Отправить сигнал (опубликовать событие)
+    emit(event, data) {
+        document.dispatchEvent(new CustomEvent(event, { detail: data }));
+    },
+    
+    // Подписаться на сигнал (слушать линию)
+    on(event, callback) {
+        document.addEventListener(event, (e) => callback(e.detail));
+    },
+
+    // Отписаться (выключить прослушку)
+    off(event, callback) {
+        document.removeEventListener(event, callback);
+    }
+};
