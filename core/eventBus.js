eaefaa17210dbb392876ca0e2bb55333b9b357fd
DIN222 <46632078+DIN222@users.noreply.github.com>
2026-08-18 11:34:30 +0300

    Update eventBus.js

--- core/eventBus.js
+++ core/eventBus.js
@@ -1,1 +1,12 @@
-
+window.EventBus = {
+    events: {},
+    on(event, callback) {
+        if (!this.events[event]) this.events[event] = [];
+        this.events[event].push(callback);
+    },
+    emit(event, data) {
+        if (this.events[event]) {
+            this.events[event].forEach(callback => callback(data));
+        }
+    }
+};
